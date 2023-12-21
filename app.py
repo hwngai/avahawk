b36b4940ba608ea8c9428e98eb2a614e2ff06425
danghung-Ptit <61653571+danghung-Ptit@users.noreply.github.com>
2023-12-21 09:37:49 +0700

    phoGPT RAG

--- app.py
+++ app.py
@@ -1,36 +1,39 @@
 import time
-import gradio as gr
 import logging
-import PyPDF2
-import docx2txt
-import openai
+import xml.etree.ElementTree as ET
+import os
+import shutil
+import requests
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.document_loaders import (
     DirectoryLoader,
     Docx2txtLoader,
     PDFMinerLoader,
     OnlinePDFLoader,
 )
-from InstructorEmbedding import INSTRUCTOR
-from langchain.embeddings import SentenceTransformerEmbeddings
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.prompts import ChatPromptTemplate
-from langchain.vectorstores.chroma import Chroma
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import PyPDFLoader
+import gradio as gr
 from langchain.document_loaders import WebBaseLoader
-from langchain.schema import Document
-import requests
-import xml.etree.ElementTree as ET
-import os
-import shutil
+from langchain.vectorstores.chroma import Chroma
+from langchain.vectorstores import DocArrayInMemorySearch
+from langchain.embeddings import SentenceTransformerEmbeddings
+from langchain.prompts import ChatPromptTemplate
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 import torch
 from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer
 
+CHROMA_PATH = "chroma"
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 logging.getLogger().setLevel(logging.CRITICAL)
+from huggingface_hub import login
 
-embedding_function = SentenceTransformerEmbeddings(model_name="bkai-foundation-models/vietnamese-bi-encoder")
-CHROMA_PATH = "chroma"
+access_token_read = "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"
+access_token_write = "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"
+login(token=access_token_read)
 
+model_kwargs = {'device': 'cuda'}
+# embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2", model_kwargs=model_kwargs)
+embedding_function = OpenAIEmbeddings(openai_api_key="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")
 
 PROMPT_TEMPLATE = """
 Trả lời câu hỏi dựa trên ngữ cảnh sau đây:
@@ -39,73 +42,89 @@ Trả lời câu hỏi dựa trên ngữ cảnh sau đây:
 
 Trả lời câu hỏi dựa trên ngữ cảnh trên: {question}
 """
-db = None
-def generate_data_store(url):
+
+db = None  # Initialize db as a global variable
+
+
+def generate_data_store(url, callback=None):
     global db
     documents = load_documents(url)
     chunks, split_text_into = split_text(documents)
     db = save_to_chroma(chunks)
+    if callback:
+        callback()
     return db, split_text_into
 
-def load_documents(url):
-    list_url = url
-    if url.endswith(".xml"):
-        response = requests.get(url)
 
-        if response.status_code == 200:
-            tree = ET.fromstring(response.content)
-            loc_elements = tree.findall(".//{http://www.sitemaps.org/schemas/sitemap/0.9}loc")
+def build_the_bot(url):
+    generate_data_store(url, callback=lambda: None)  # Use a callback to set db
+    return "Successfully built the bot!"
 
-            list_url = [loc_element.text for loc_element in loc_elements]
-            print(len(list_url))
-        else:
-            print("Failed to retrieve sitemap. Status code:", response.status_code)
-    loader = WebBaseLoader(list_url)
+
+def load_documents(documents):
+    if type(documents) == str:
+        list_url = documents
+        if documents.endswith(".xml"):
+            response = requests.get(documents)
+
+            if response.status_code == 200:
+                tree = ET.fromstring(response.content)
+                loc_elements = tree.findall(".//{http://www.sitemaps.org/schemas/sitemap/0.9}loc")
+
+                list_url = [loc_element.text for loc_element in loc_elements]
+                print(len(list_url))
+            else:
+                print("Failed to retrieve sitemap. Status code:", response.status_code)
+        loader = WebBaseLoader(list_url)
+    else:
+        loader = OnlinePDFLoader(documents)
+
     documents = loader.load()
+    print(documents)
     return documents
 
+
 def split_text(documents):
+    default_chunk_separators = ['\n\n', '\n', ' ', '']
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=200,
         length_function=len,
         add_start_index=True,
+        separators=default_chunk_separators,
     )
     chunks = text_splitter.split_documents(documents)
     print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
     return chunks, f"Split {len(documents)} documents into {len(chunks)} chunks."
 
-def save_to_chroma(chunks):
-    if os.path.exists(CHROMA_PATH):
-        shutil.rmtree(CHROMA_PATH)
 
+def save_to_chroma(chunks):
     db = Chroma.from_documents(
-        chunks, embedding=embedding_function, persist_directory=CHROMA_PATH
+        chunks, embedding=embedding_function
     )
 
-    db.persist()
     return db
 
-
-def build_the_bot(url):
-    global db
-    db,  split_text_into = generate_data_store(url)
-    return split_text_into
 
 def input_question(question):
     global db
     if not question or not db:
         return "Error: Invalid question or database."
 
-    results = db.similarity_search_with_relevance_scores(question, k=10)
-    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    retriever = db.as_retriever(search_type="mmr", search_kwargs={"k": 5, "fetch_k": 10})
+    docs = retriever.get_relevant_documents(question)
+    context_text = "\n\n---\n\n".join([doc.page_content for doc in docs])
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+
     prompt = prompt_template.format(context=context_text, question=question)
-    # prompt = generate_response(model, tokenizer, prompt)
+    print(prompt)
+    prompt = generate_response(model, tokenizer, prompt)
     return prompt
+
 
 def user(user_message, history):
     return "", history + [[user_message, None]]
+
 
 def chat(history):
     if history[-1][1] is None or history[-1][1] == "":
@@ -116,41 +135,44 @@ def chat(history):
             time.sleep(0.005)
             yield history
 
+
 def generate_response_avahawk(input_text):
-    if input_text is not None or input_text != "":
+    if input_text:
         bot_message = input_question(input_text)
         return bot_message
 
-# def generate_response(model, tokenizer, instruction, max_new_tokens=1024, temperature=1.0, top_k=50, top_p=0.9):
-#     PROMPT_TEMPLATE = "### Câu hỏi:\n{instruction}\n\n### Trả lời:"
-#     input_prompt = PROMPT_TEMPLATE.format_map({"instruction": instruction})
-#
-#     input_ids = tokenizer(input_prompt, return_tensors="pt")
-#
-#     outputs = model.generate(
-#         inputs=input_ids["input_ids"].to("cuda"),
-#         attention_mask=input_ids["attention_mask"].to("cuda"),
-#         do_sample=True,
-#         temperature=temperature,
-#         top_k=top_k,
-#         top_p=top_p,
-#         max_new_tokens=max_new_tokens,
-#         eos_token_id=tokenizer.eos_token_id,
-#         pad_token_id=tokenizer.pad_token_id
-#     )
-#
-#     response = tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
-#     response = response.split("### Trả lời:")[1]
-#
-#     return response
-#
-#
-# model_path = "vinai/PhoGPT-7B5-Instruct"
-# config = AutoConfig.from_pretrained(model_path, trust_remote_code=True)
-# config.init_device = "cuda"
-# model = AutoModelForCausalLM.from_pretrained(model_path, config=config, torch_dtype=torch.bfloat16, trust_remote_code=True)
-# model.eval()
-# tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+
+def generate_response(model, tokenizer, instruction, max_new_tokens=1024, temperature=1.0, top_k=50, top_p=0.9):
+    PROMPT_TEMPLATE = "### Câu hỏi:\n{instruction}\n\n### Trả lời:"
+    input_prompt = PROMPT_TEMPLATE.format_map({"instruction": instruction})
+
+    input_ids = tokenizer(input_prompt, return_tensors="pt")
+
+    outputs = model.generate(
+        inputs=input_ids["input_ids"].to("cuda"),
+        attention_mask=input_ids["attention_mask"].to("cuda"),
+        do_sample=True,
+        temperature=temperature,
+        top_k=top_k,
+        top_p=top_p,
+        max_new_tokens=max_new_tokens,
+        eos_token_id=tokenizer.eos_token_id,
+        pad_token_id=tokenizer.pad_token_id
+    )
+
+    response = tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
+    response = response.split("### Trả lời:")[1]
+
+    return response
+
+
+model_path = "vinai/PhoGPT-7B5-Instruct"
+config = AutoConfig.from_pretrained(model_path, trust_remote_code=True)
+config.init_device = "cuda"
+model = AutoModelForCausalLM.from_pretrained(model_path, config=config, torch_dtype=torch.bfloat16,
+                                             trust_remote_code=True)
+model.eval()
+tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
 
 with gr.Blocks() as demo:
     gr.Markdown('<h2 style="font-style: italic;">AvaHawk: Document Chatbot with PhoGPT VinAI RAG Integration</h2>')
@@ -175,32 +197,23 @@ with gr.Blocks() as demo:
                     Elevate your document interactions with AvaHawk and experience a new level of conversational depth and intelligence.
                 ''')
 
-        document_file = gr.Textbox(label="Urls (comma-separated)", lines=2)
+        # document_file = gr.File(label="Upload your document")
+        document_file = gr.Textbox(label="Document url")
+
         text_output = gr.Textbox(label="Document content")
         text_button = gr.Button("Build the Bot!!!")
         text_button.click(build_the_bot, [document_file], text_output)
+
     with gr.Tab("AvaHawk"):
         chatbot = gr.Chatbot(avatar_images=["assets/user.png", "assets/2023-12-12 21.32.53.jpg"], height=450)
         msg = gr.Textbox(label="Enter your query here")
         clear = gr.ClearButton([msg, chatbot])
 
-        def user(user_message, history):
-            return "", history + [[user_message, None]]
-
-
-        def bot(history):
-            bot_message = generate_response_avahawk(history[-1][0])
-            history[-1][1] = ""
-            for character in bot_message:
-                history[-1][1] += character
-                time.sleep(0.002)
-                yield history
-
-
         msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(
-            bot, chatbot, chatbot
+            chat, chatbot, chatbot
         )
         clear.click(lambda: None, None, chatbot, queue=False)
+
     with gr.Tab("PhoGPT"):
         chatbot = gr.Chatbot(avatar_images=["assets/user.png", "assets/_wjiAzCq_400x400.jpeg"], height=450)
         msg = gr.Textbox(label="Enter your query here")
@@ -212,8 +225,7 @@ with gr.Blocks() as demo:
 
 
         def bot(history):
-            # bot_message = generate_response(model, tokenizer, history[-1][0])
-            bot_message = generate_response_avahawk(history[-1][0])
+            bot_message = generate_response(model, tokenizer, history[-1][0])
             history[-1][1] = ""
             for character in bot_message:
                 history[-1][1] += character
